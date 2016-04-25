980783f16d947579142f780caf4e565dc35dd350
diorge <diorge.nla@hotmail.com>
2016-04-25 12:21:47 -0300

    CLI parameter to search the N most frequent pairs

--- naivefrequency.cpp
+++ naivefrequency.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 #include <utility>
+#include <algorithm>
 
 
 struct SvoRow {
@@ -23,17 +24,40 @@ struct hashPair {
 };
 
 
+bool compareTuples(std::tuple<std::string, std::string, int> a,
+                     std::tuple<std::string, std::string, int> b) {
+   return std::get<2>(a) > std::get<2>(b);
+}
+
+
 int main(int argc, char** argv) {
    std::ifstream input("sample.svo");
 
+   if (argc < 2) {
+      std::cout << "Unspecified N value" << std::endl;
+      return -1;
+   }
+
+
+   int n = std::stoi(argv[1]);
    SvoRow row;
    std::unordered_map<std::pair<std::string, std::string>, int, hashPair> pairs;
+   std::vector<std::tuple<std::string, std::string, int> > ordered;
 
    while (input >> row.s >> row.v >> row.o >> row.n) {
       pairs[std::make_pair(row.s, row.o)] += row.n;
    }
+
    for (const auto &data : pairs) {
-      std::cout << "(" << data.first.first << ", " << data.first.second
-                << ") = " << data.second << "\n";
+      ordered.push_back(std::make_tuple(data.first.first,
+                                          data.first.second, data.second));
+   }
+
+   std::partial_sort(ordered.begin(), ordered.begin() + n,
+                     ordered.end(), compareTuples);
+
+   for (auto it = ordered.begin(); it != ordered.begin() + n; ++it) {
+      std::cout << "(" << std::get<0>(*it) << ", " << std::get<1>(*it)
+                  << ") = " << std::get<2>(*it) << "\n";
    }
 }
